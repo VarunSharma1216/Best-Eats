b9dc9672a062e667236662b7f8d69f9656a6baf6
vas0011 <156358381+vas0011@users.noreply.github.com>
2025-06-28 18:42:53 -0700

    Added Category component to App and implemented filtering functionality in Food component for type and price.

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { data } from "../data/data";
 
 const Food = () => {
-  console.log(data);
+  //console.log(data);
+  const [foods, setFoods] = useState(data);
+
+  // Filter Type
+  const filterType = (category) => {
+    setFoods(
+      data.filter((item) => {
+        return item.category === category;
+      })
+    );
+  };
+
+  // Filter Price
+  const filterPrice = (price) => {
+    setFoods(
+      data.filter((item) => {
+        return item.price === price;
+      })
+    );
+  };
   return (
     <div className="max-w-[1640px] mx-auto p-4 py-12">
       <h1 className="text-orange-600 font-bold text-4xl text-center">
@@ -10,28 +29,96 @@ const Food = () => {
       </h1>
 
       {/*Filter Row*/}
-      <div>
+      <div className="flex flex-col lg:flex-row justify-between">
         {/*Filter type*/}
         <div>
-          <p>Filter Type</p>
-          <div>
-            <button>All</button>
-            <button>Pizza</button>
-            <button>Burger</button>
-            <button>Salad</button>
-            <button>Chicken</button>
+          <p className="font-bold text-gray-700">Filter Type</p>
+          <div className="flex justify-between flex-wrap">
+            <button
+              onClick={() => setFoods(data)}
+              className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
+              All
+            </button>
+            <button
+              onClick={() => filterType("pizza")}
+              className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
+              Pizza
+            </button>
+            <button
+              onClick={() => filterType("burger")}
+              className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
+              Burger
+            </button>
+            <button
+              onClick={() => filterType("salad")}
+              className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
+              Salad
+            </button>
+            <button
+              onClick={() => filterType("chicken")}
+              className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
+              Chicken
+            </button>
           </div>
         </div>
         {/*Filter price*/}
         <div>
-          <p>Filter Price</p>
-          <div>
-            <button>$</button>
-            <button>$$</button>
-            <button>$$$</button>
-            <button>$$$$</button>
+          <p className="font-bold text-gray-700">Filter Price</p>
+          <div className="flex justify-between max-w-[390px] w-full">
+            <button
+              onClick={() => filterPrice("$")}
+              className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
+              $
+            </button>
+            <button
+              onClick={() => filterPrice("$$")}
+              className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
+              $$
+            </button>
+            <button
+              onClick={() => filterPrice("$$$")}
+              className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
+              $$$
+            </button>
+            <button
+              onClick={() => filterPrice("$$$$")}
+              className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
+              $$$$
+            </button>
           </div>
         </div>
+      </div>
+      {/*Display foods*/}
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4">
+        {foods.map((item, index) => (
+          <div
+            key={index}
+            className="border shadow-lg rounded-lg hover:scale-105 duration-300"
+          >
+            <img
+              src={item.image}
+              alt={item.name}
+              className="w-full h-[200px] object-cover rounded-t-lg"
+            />
+            <div className="flex justify-between px-2 py-4">
+              <p className="font-bold">{item.name}</p>
+              <p>
+                <span className="bg-orange-500 text-white p-1 rounded-full">
+                  {item.price}
+                </span>
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
